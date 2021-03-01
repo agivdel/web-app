c0482c9d311ebaf599b660288b1081833f80339f
agivdel <agph@yandex.ru>
2021-03-01 11:38:56 +0300

    delete unused method

--- src/main/java/agivdel/webApp1311/utils/ConnectionManager.java
+++ src/main/java/agivdel/webApp1311/utils/ConnectionManager.java
@@ -1,25 +1,11 @@
 package agivdel.webApp1311.utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionManager {
-
-    private Properties getProperties() {
-        Properties property = new Properties();
-        try {
-            FileInputStream input = new FileInputStream("src/main/resources/application.properties");
-            property.load(input);
-        } catch (IOException e) {
-            System.err.println("failed to load the file 'application.properties'");
-            e.printStackTrace();
-        }
-        return property;
-    }
 
     public Connection getConnection() {
         Connection connection = null;
