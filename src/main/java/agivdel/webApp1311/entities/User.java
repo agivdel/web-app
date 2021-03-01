a3397e8c4d70d1f2ebf4b129ce5f4936a10e80c3
agivdel <agph@yandex.ru>
2021-03-01 14:48:34 +0300

    remove method equals()

--- src/main/java/agivdel/webApp1311/entities/User.java
+++ src/main/java/agivdel/webApp1311/entities/User.java
@@ -55,14 +55,6 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id;
-    }
-
-    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
