f4d32d09c5e429884997bfddc8e196ef05e21724
agivdel <agph@yandex.ru>
2021-03-01 19:31:29 +0300

    add equals() and hashCode()

--- src/main/java/agivdel/webApp1311/entities/User.java
+++ src/main/java/agivdel/webApp1311/entities/User.java
@@ -1,5 +1,7 @@
 package agivdel.webApp1311.entities;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String username;
@@ -52,6 +54,19 @@ public class User {
 
     public void setBalance(long balance) {
         this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
