906f6ff3307513d49561edd3a1fc932c58cb5cde
agivdel <agph@yandex.ru>
2021-03-01 11:34:03 +0300

    rewrite ConnectionManager class: add method for reading from application.properties

--- src/main/java/agivdel/webApp1311/utils/ConnectionManager.java
+++ src/main/java/agivdel/webApp1311/utils/ConnectionManager.java
@@ -1,12 +1,27 @@
 package agivdel.webApp1311.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionManager {
-    public static Connection getConnection() {
+
+    private Properties getProperties() {
+        Properties property = new Properties();
+        try {
+            FileInputStream input = new FileInputStream("src/main/resources/application.properties");
+            property.load(input);
+        } catch (IOException e) {
+            System.err.println("failed to load the file 'application.properties'");
+            e.printStackTrace();
+        }
+        return property;
+    }
+
+    public Connection getConnection() {
         Connection connection = null;
         Properties property = PropertiesReader.getProperties();
         try {
@@ -17,12 +32,20 @@ public class ConnectionManager {
                     property.getProperty("database.password")
             );
         } catch (SQLException e) {
-            System.err.println("0");
             System.err.println("SQLException: " + e.getSQLState());
-            e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.err.println("database driver not found");
         }
         return connection;
+    }
+
+    public void close(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
