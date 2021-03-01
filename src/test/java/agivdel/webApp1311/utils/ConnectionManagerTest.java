e5e4c31ca4dfd692b066f1f35c14ca5a1eb296bc
agivdel <agph@yandex.ru>
2021-03-01 11:36:29 +0300

    add ConnectionManagerTest class

--- src/test/java/agivdel/webApp1311/utils/ConnectionManagerTest.java
+++ src/test/java/agivdel/webApp1311/utils/ConnectionManagerTest.java
@@ -1,31 +1,20 @@
 package agivdel.webApp1311.utils;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.sql.Connection;
 import java.sql.SQLException;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConnectionManagerTest {
-    private static Connection con;
-
-    @BeforeEach
-    public void init() {
-        con = new ConnectionManager().getConnection();
-    }
-
-    @AfterEach
-    public void close() {
-        new ConnectionManager().close(con);
-    }
 
     @Test
     public void getDBConnectionTest() throws SQLException {
+        Connection con = new ConnectionManager().getConnection();
+
         assertTrue(con.isValid(1));
         assertFalse(con.isClosed());
+
+        new ConnectionManager().close(con);
     }
 }
