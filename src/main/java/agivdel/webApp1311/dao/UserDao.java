c813c3fd5eb5dfff148c5720340095d207d01ac3
agivdel <agph@yandex.ru>
2021-03-01 14:20:30 +0300

    make global final String vars

--- src/main/java/agivdel/webApp1311/dao/UserDao.java
+++ src/main/java/agivdel/webApp1311/dao/UserDao.java
@@ -6,13 +6,17 @@ import java.lang.String;
 import java.sql.*;
 
 public class UserDao {
+    private static final String INSERT_INTO_USERS_USERNAME = "INSERT INTO users (username) VALUES (?)";
+    private static final String UPDATE_USERS_SET_PASSWORD = "UPDATE users SET password=? WHERE id=?";
+    private static final String SELECT_ID_PASSWORD_FROM_USERS = "SELECT id, password FROM users WHERE username=?";
+    private static final String INSERT_INTO_BALANCES_ID_BALANCE = "INSERT INTO balances (id, balance) VALUES (?,?)";
+    private static final String UPDATE_BALANCES_SET_BALANCE = "UPDATE balances SET balance=? WHERE id=?";
+    private static final String SELECT_BALANCE_FROM_BALANCES = "SELECT balance FROM balances WHERE id=?";
 
     public void insertUser(Connection con, User user) throws Exception {
-        String userSqlInsert = "INSERT INTO users (username) VALUES (?)";
-
         // Перед возвратом Connection con в пул все Statement'ы и ResultSet'ы,
         // полученные с помощью этого соединения, автоматически закрываются в соответствии с API
-        PreparedStatement pstUsers = con.prepareStatement(userSqlInsert, new String[]{"id"});
+        PreparedStatement pstUsers = con.prepareStatement(INSERT_INTO_USERS_USERNAME, new String[]{"id"});
         pstUsers.setString(1, user.getUsername());
         pstUsers.executeUpdate();
 
@@ -24,18 +28,14 @@ public class UserDao {
     }
 
     public void updatePassword(Connection con, int userId, String password) throws SQLException {
-        String userSqlInsert = "UPDATE users SET password=? WHERE id=?";
-
-        PreparedStatement pstUsers = con.prepareStatement(userSqlInsert);
+        PreparedStatement pstUsers = con.prepareStatement(UPDATE_USERS_SET_PASSWORD);
         pstUsers.setString(1, password);
         pstUsers.setInt(2, userId);
         pstUsers.executeUpdate();
     }
 
     public User selectUser(Connection con, String username) throws Exception {
-        String userSqlSelect = "SELECT id, password FROM users WHERE username=?";
-
-        PreparedStatement pstUser = con.prepareStatement(userSqlSelect);
+        PreparedStatement pstUser = con.prepareStatement(SELECT_ID_PASSWORD_FROM_USERS);
         pstUser.setString(1, username);
 
         ResultSet resultSetUser = pstUser.executeQuery();
@@ -48,27 +48,21 @@ public class UserDao {
     }
 
     public void insertBalance(Connection con, int userId, long balance) throws SQLException {
-        String balancesSqlUpdate = "INSERT INTO balances (id, balance) VALUES (?,?)";
-
-        PreparedStatement pstBalances = con.prepareStatement(balancesSqlUpdate);
+        PreparedStatement pstBalances = con.prepareStatement(INSERT_INTO_BALANCES_ID_BALANCE);
         pstBalances.setInt(1, userId);
         pstBalances.setLong(2, balance);
         pstBalances.executeUpdate();
     }
 
     public void updateBalance(Connection con, int userId, long balance) throws SQLException {
-        String balancesSqlUpdate = "UPDATE balances SET balance=? WHERE id=?";
-
-        PreparedStatement pstBalances = con.prepareStatement(balancesSqlUpdate);
+        PreparedStatement pstBalances = con.prepareStatement(UPDATE_BALANCES_SET_BALANCE);
         pstBalances.setLong(1, balance);
         pstBalances.setInt(2, userId);
         pstBalances.executeUpdate();
     }
 
     public Long selectBalance(Connection con, int userId) throws Exception {
-        String balanceSqlSelect = "SELECT balance FROM balances WHERE id=?";
-
-        PreparedStatement pstBalance = con.prepareStatement(balanceSqlSelect);
+        PreparedStatement pstBalance = con.prepareStatement(SELECT_BALANCE_FROM_BALANCES);
         pstBalance.setInt(1, userId);
 
         ResultSet resultSetBalance = pstBalance.executeQuery();
