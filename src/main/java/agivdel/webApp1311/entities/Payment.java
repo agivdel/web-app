e349273cff18979ac593e4e20a4aa219aaf21c41
agivdel <agph@yandex.ru>
2021-03-01 18:30:15 +0300

    add method selectPayment() in UserDao class; add Payment class

---
+++ src/main/java/agivdel/webApp1311/entities/Payment.java
@@ -0,0 +1,60 @@
+package agivdel.webApp1311.entities;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private int id;
+    private int userId;
+    private Long payment;
+    private Timestamp paymentTime;
+
+    public Payment(int id, int userId, Long payment, Timestamp paymentTime) {
+        this.id = id;
+        this.userId = userId;
+        this.payment = payment;
+        this.paymentTime = paymentTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Long getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Long payment) {
+        this.payment = payment;
+    }
+
+    public Timestamp getPaymentTime() {
+        return paymentTime;
+    }
+
+    public void setPaymentTime(Timestamp paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", payment=" + payment +
+                ", paymentTime=" + paymentTime +
+                '}';
+    }
+}
