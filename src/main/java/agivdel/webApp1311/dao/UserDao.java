ab5a20c25e5209eff266f5073dcb3ce14cf55b4f
agivdel <agph@yandex.ru>
2021-03-01 19:24:39 +0300

    add method selectPayment() in UserDao class

--- src/main/java/agivdel/webApp1311/dao/UserDao.java
+++ src/main/java/agivdel/webApp1311/dao/UserDao.java
@@ -1,23 +1,27 @@
 package agivdel.webApp1311.dao;
 
+import agivdel.webApp1311.entities.Payment;
 import agivdel.webApp1311.entities.User;
 
 import java.lang.String;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
-    private static final String INSERT_INTO_USERS_USERNAME = "INSERT INTO users (username) VALUES (?)";
-    private static final String UPDATE_USERS_SET_PASSWORD = "UPDATE users SET password=? WHERE id=?";
-    private static final String SELECT_ID_PASSWORD_FROM_USERS = "SELECT id, password FROM users WHERE username=?";
-    private static final String INSERT_INTO_BALANCES_ID_BALANCE = "INSERT INTO balances (id, balance) VALUES (?,?)";
-    private static final String UPDATE_BALANCES_SET_BALANCE = "UPDATE balances SET balance=? WHERE id=?";
-    private static final String SELECT_BALANCE_FROM_BALANCES = "SELECT balance FROM balances WHERE id=?";
-    private static final String INSERT_INTO_PAYMENTS_ID_PAYMENT = "INSERT INTO payments (user_id, payment) VALUES (?,?)";
+    private static final String INSERT_USERNAME = "INSERT INTO users (username) VALUES (?)";
+    private static final String UPDATE_PASSWORD_WHERE_ID = "UPDATE users SET password=? WHERE id=?";
+    private static final String SELECT_ID_PASSWORD_WHERE_USERNAME = "SELECT id, password FROM users WHERE username=?";
+    private static final String INSERT_ID_BALANCE = "INSERT INTO balances (id, balance) VALUES (?,?)";
+    private static final String UPDATE_BALANCE_WHERE_ID = "UPDATE balances SET balance=? WHERE id=?";
+    private static final String SELECT_BALANCE_WHERE_ID = "SELECT balance FROM balances WHERE id=?";
+    private static final String INSERT_ID_PAYMENT = "INSERT INTO payments (user_id, payment) VALUES (?,?)";
+    private static final String SELECT_ALL_FROM_PAYMENTS_WHERE_USER_ID = "SELECT * FROM payments WHERE user_id=?";
 
     public void insertUser(Connection con, User user) throws Exception {
         // Перед возвратом Connection con в пул все Statement'ы и ResultSet'ы,
         // полученные с помощью этого соединения, автоматически закрываются в соответствии с API
-        PreparedStatement pstUsers = con.prepareStatement(INSERT_INTO_USERS_USERNAME, new String[]{"id"});
+        PreparedStatement pstUsers = con.prepareStatement(INSERT_USERNAME, new String[]{"id"});
         pstUsers.setString(1, user.getUsername());
         pstUsers.executeUpdate();
 
@@ -29,19 +33,19 @@ public class UserDao {
     }
 
     public void updatePassword(Connection con, int userId, String password) throws SQLException {
-        PreparedStatement pstUsers = con.prepareStatement(UPDATE_USERS_SET_PASSWORD);
+        PreparedStatement pstUsers = con.prepareStatement(UPDATE_PASSWORD_WHERE_ID);
         pstUsers.setString(1, password);
         pstUsers.setInt(2, userId);
         pstUsers.executeUpdate();
     }
 
     public User selectUser(Connection con, String username) throws SQLException {
-        PreparedStatement pstUser = con.prepareStatement(SELECT_ID_PASSWORD_FROM_USERS);
+        PreparedStatement pstUser = con.prepareStatement(SELECT_ID_PASSWORD_WHERE_USERNAME);
         pstUser.setString(1, username);
 
         ResultSet resultSetUser = pstUser.executeQuery();
         if (!resultSetUser.next()) {
-            return new User();
+            return null;
         }
         int userId = resultSetUser.getInt(1);
         String password = resultSetUser.getString(2);
@@ -49,21 +53,21 @@ public class UserDao {
     }
 
     public void insertBalance(Connection con, int userId, long balance) throws SQLException {
-        PreparedStatement pstBalances = con.prepareStatement(INSERT_INTO_BALANCES_ID_BALANCE);
+        PreparedStatement pstBalances = con.prepareStatement(INSERT_ID_BALANCE);
         pstBalances.setInt(1, userId);
         pstBalances.setLong(2, balance);
         pstBalances.executeUpdate();
     }
 
     public void updateBalance(Connection con, int userId, long balance) throws SQLException {
-        PreparedStatement pstBalances = con.prepareStatement(UPDATE_BALANCES_SET_BALANCE);
+        PreparedStatement pstBalances = con.prepareStatement(UPDATE_BALANCE_WHERE_ID);
         pstBalances.setLong(1, balance);
         pstBalances.setInt(2, userId);
         pstBalances.executeUpdate();
     }
 
     public Long selectBalance(Connection con, int userId) throws Exception {
-        PreparedStatement pstBalance = con.prepareStatement(SELECT_BALANCE_FROM_BALANCES);
+        PreparedStatement pstBalance = con.prepareStatement(SELECT_BALANCE_WHERE_ID);
         pstBalance.setInt(1, userId);
 
         ResultSet resultSetBalance = pstBalance.executeQuery();
@@ -74,9 +78,27 @@ public class UserDao {
     }
 
     public void insertPayment(Connection con, int userId, long payment) throws SQLException {
-        PreparedStatement pstBalances = con.prepareStatement(INSERT_INTO_PAYMENTS_ID_PAYMENT);
+        PreparedStatement pstBalances = con.prepareStatement(INSERT_ID_PAYMENT);
         pstBalances.setInt(1, userId);
         pstBalances.setLong(2, payment);
         pstBalances.executeUpdate();
+    }
+
+    public List<Payment> selectPayments(Connection con, int userId) throws Exception {
+        PreparedStatement pstPayment = con.prepareStatement(SELECT_ALL_FROM_PAYMENTS_WHERE_USER_ID);
+        pstPayment.setInt(1, userId);
+
+        ResultSet resultSetPayment = pstPayment.executeQuery();
+        List<Payment> payments = new ArrayList<>();
+        while (resultSetPayment.next()) {
+            Payment payment = new Payment(
+                    resultSetPayment.getInt(1),
+                    resultSetPayment.getInt(2),
+                    resultSetPayment.getLong(3),
+                    resultSetPayment.getTimestamp(4)
+            );
+            payments.add(payment);
+        }
+        return payments;
     }
 }
