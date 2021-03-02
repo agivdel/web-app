ecbb0edf89f8adf3126a8e7dad57bb9eccf83470
agivdel <agph@yandex.ru>
2021-03-03 02:13:11 +0300

    change expected exceptions in the methods

--- src/main/java/agivdel/webApp1311/password/PBKDF2.java
+++ src/main/java/agivdel/webApp1311/password/PBKDF2.java
@@ -3,7 +3,9 @@ package agivdel.webApp1311.password;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -14,23 +16,23 @@ public class PBKDF2 {
     private static final String algorithm = "PBKDF2WithHmacSHA1";
     private static final String pseudoRNG = "SHA1PRNG";
 
-    public static String getSaltedHash(String password) throws Exception {
+    public static String getSaltedHash(String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
         byte[] salt = SecureRandom.getInstance(pseudoRNG).generateSeed(saltLength);
         return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
     }
 
-    public static boolean compare(String password, String storedPassword) throws Exception {
+    public static boolean compare(String password, String storedPassword) throws InvalidKeySpecException, NoSuchAlgorithmException {
         String[] saltAndPassword = storedPassword.split("\\$");
         if (saltAndPassword.length != 2) {
-            throw new IllegalStateException("The stored password have the form 'salt$hash'");
+            throw new IllegalStateException("the stored password have the form 'salt$hash'");
         }
         String hashOfInput = hash(password, Base64.decodeBase64(saltAndPassword[0]));
         return hashOfInput.equals(saltAndPassword[1]);
     }
 
-    private static String hash(String password, byte[] salt) throws Exception {
+    private static String hash(String password, byte[] salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
         if (password == null || password.length() == 0) {
-            throw new IllegalArgumentException("Empty passwords are not supported");
+            throw new IllegalArgumentException("empty passwords are not supported");
         }
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(algorithm);
         SecretKey key = keyFactory.generateSecret(new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength));
