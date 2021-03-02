551a1ea809b9a29c9ccabaabf7bd2ec1f2f06798
agivdel <agph@yandex.ru>
2021-03-02 16:46:33 +0300

    rewrite method doPost with methods of Check class

--- src/main/java/agivdel/webApp1311/servlets/LogInServlet.java
+++ src/main/java/agivdel/webApp1311/servlets/LogInServlet.java
@@ -1,5 +1,6 @@
 package agivdel.webApp1311.servlets;
 
+import agivdel.webApp1311.service.Check;
 import agivdel.webApp1311.service.Service;
 import agivdel.webApp1311.entities.User;
 import jakarta.servlet.ServletException;
@@ -28,48 +29,13 @@ public class LogInServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        User user = userCheck(req, resp, username, password);
+        Check check = new Check(this, req, resp);
+        check.userValid(username, password);
+        check.userExist(username, password);
+        User user = check.password(username, password);
 
-        //сохраняем информацию пользователя в сессию
         HttpSession session = req.getSession();
         session.setAttribute("authenticatedUser", user);
         resp.sendRedirect(req.getContextPath() + "payment.jsp");
-    }
-
-    private User userCheck(HttpServletRequest req, HttpServletResponse resp, String username, String password) throws ServletException, IOException {
-        User newUser = new User(username, password);
-        Service service = new Service();
-        if (isNotValidEnteredData(newUser)) {
-            repeatLogIn(req, resp, newUser, "enter not empty username and password");
-        }
-        try {
-            if (!service.isUserExists(username)) {
-                repeatLogIn(req, resp, newUser, "this username was not found");
-            }
-        } catch (Exception e) {
-            repeatLogIn(req, resp, newUser, e.getMessage());
-        }
-        try {
-            if (!service.authentication(username, password)) {
-                repeatLogIn(req, resp, newUser, "invalid username-password pair");
-            }
-            newUser = service.findUser(username);
-        } catch (Exception e) {
-            repeatLogIn(req, resp, newUser, e.getMessage());
-        }
-        return newUser;
-    }
-
-    private boolean isNotValidEnteredData(User user) {
-        return user.getUsername() == null
-                || user.getPassword() == null
-                || user.getUsername().length() == 0
-                || user.getPassword().length() == 0;
-    }
-
-    private void repeatLogIn(HttpServletRequest req, HttpServletResponse resp, User newUser, String errorMessage) throws ServletException, IOException {
-        req.setAttribute("errorMassage", errorMessage);
-        req.setAttribute("user", newUser);
-        this.getServletContext().getRequestDispatcher("views/login.jsp").forward(req, resp);
     }
 }
