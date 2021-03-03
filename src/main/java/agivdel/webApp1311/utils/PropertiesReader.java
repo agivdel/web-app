fb315e0f812af9567137f4b88bf504bd88167748
agivdel <agph@yandex.ru>
2021-03-03 09:46:08 +0300

    change way of load the file

--- src/main/java/agivdel/webApp1311/utils/PropertiesReader.java
+++ src/main/java/agivdel/webApp1311/utils/PropertiesReader.java
@@ -1,14 +1,14 @@
 package agivdel.webApp1311.utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesReader {
     public static Properties getProperties() {
         Properties property = new Properties();
         try {
-            FileInputStream input = new FileInputStream("src/main/resources/application.properties");
+            InputStream input = PropertiesReader.class.getClassLoader().getResourceAsStream("application.properties");
             property.load(input);
         } catch (IOException e) {
             System.err.println("failed to load the file 'application.properties'");
